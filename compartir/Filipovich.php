e26cb10af38842620290652aa35cf8a640f8ddb4
Santiago Romero <elsantoposada@gmail.com>
2022-04-29 23:25:40 -0300

    reparamdp errpres

--- compartir/Filipovich.php
+++ compartir/Filipovich.php
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-<?php echo "gordo lesbiano";
-echo "scubi du";
-echo "el filipoper se embuta yogurt en la pija porque coge perros";
-=======
 <?php echo "nada de mensajes malintencionados";
->>>>>>> romero
