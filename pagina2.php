32bab55ef29960bfa426f6c3ee8bebb4fb204946
Nicolás Filipovich <filiporovich777@gmail.com>
2022-05-24 02:08:07 -0300

    mi tareita ;)

---
+++ pagina2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chimilai</title>
+</head>
+
+<body>
+    <h1>paginas1 recibe datos get</h1>
+    <?php $param01 = $_GET['param01']; 
+    $param02       = $_GET['param02'];
+    ?>
+</body>
+
+</html>
