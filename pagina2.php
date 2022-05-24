6d104ec3cdcb5c906ff47c5c71f54904a4c26572
Nicolás Filipovich <filiporovich777@gmail.com>
2022-05-24 02:41:35 -0300

    Update pagina2.php

--- pagina2.php
+++ pagina2.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chimilai</title>
+    <title>pagina 2</title>
 </head>
-
 <body>
-    <h1>paginas1 recibe datos get</h1>
-    <?php $param01 = $_GET['param01']; 
-    $param02       = $_GET['param02'];
-    ?>
+    <h1>Crear Persona</h1>
+    <form action="recibir.php" method="post">
+    <label for="edad">edad</label>
+    <input type="text" name="edad" id="edad" value="25">
+    <label for="">nombre</label>
+    <input type="text" name="nombre">
+    <input type="submit" value="enviar">
+    </form>
 </body>
-
 </html>
