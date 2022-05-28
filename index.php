f68c5a46b8dadfb218d77678bc36f0de08474fb9
Santiago <elsantoposada@gmail.com>
2022-05-27 21:59:15 -0300

    SantiagoRomero

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sorete</title>
+</head>
+
+<body>
+  
+  <?php echo "Scooby Dooby Dooooo"; ?>
+  <ul>
+    <li><a href="pagina1.php?param01=hello&param02=world">pagina</a></li>
+    <li><a href="pagina2.php">pagina2</a></li>
+  </ul>
+  
+</body>
+</html>
---
+++ pagina2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorete</title>
+</head>
+<body>
+    <h1>Crear Persona</h1>
+    <form action="recibir.php" method="post">
+    <label for="edad">edad</label>
+    <input type="text" name="edad" id="edad" value="24">
+    <label for="">nombre</label>
+    <input type="text" name="nombre">
+    <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
